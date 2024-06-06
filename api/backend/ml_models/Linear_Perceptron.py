3830ea64f81443d92891ff70284ae774c269c734
Paolo Lanaro <paololanaro04@gmail.com>
2024-06-06 11:09:16 +0200

    added a cutoff for the positivity scores

--- api/backend/ml_models/Linear_Perceptron.py
+++ api/backend/ml_models/Linear_Perceptron.py
@@ -38,8 +38,8 @@ def predict(country):
         sentiment (float): value of +/- 1 that allows for the user to understand general sentiment of their country
     """
     safety_score = df_ss.loc[df_ss['Country'] == country]['Safety Index']
-    logger.info(f'safety_score = {safety_score}')
-    X = np.concatenate((1, safety_score), axis=None)
+    logger.info(f'safety_score = {safety_score}') # records and stores the current country
+    X = np.concatenate((1, safety_score), axis=None) # [1 safety_score]
     
     logger.info(f'current X= {X}')
 
@@ -71,9 +71,13 @@ def predict(country):
     logging.info(f'w datatype: {type(params_array)}')
     logging.info('')
     
-    Xp = np.column_stack([np.ones(np.array(df['Safety Index']).shape[0]), np.array(df['Safety Index'])])
+    prediction = np.dot(X, params_array)
+
+    # less then -1.3 is good, greater is bad
+
+    anal = -1 if prediction >= -.13 else 1
     
-    return np.where(np.dot(X, params_array) >= 0, -1, 1)[0]
+    return anal
 
 def linear_perceptron(X, y, w, alpha = 1, max_iter = None):
     """
