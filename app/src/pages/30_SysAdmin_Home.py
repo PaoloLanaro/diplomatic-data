ded736f37ebc01054151239ec6797596ccc72832
Nia Quinn <niaaquinn@gmail.com>
2024-06-10 02:31:27 -0700

    fixing some of the formatting for displaying training data

--- app/src/pages/30_SysAdmin_Home.py
+++ app/src/pages/30_SysAdmin_Home.py
@@ -2,6 +2,7 @@ import requests
 
 import streamlit as st
 from modules.nav import SideBarLinks
+import numpy as np
 
 st.set_page_config(layout="wide")
 
@@ -29,6 +30,5 @@ if st.button(
 ):
     training = requests.get("http://api:4000/models/train_prediction2")
     st.write(
-        f"This is how we trained the random forest classifier. Here's how we trained the X values:\n{training.json()['x_train']} and here are the y values they were trained against:\n{training.json()['y_train']}."
+        f"This is how we trained the random forest classifier. Here's some of the first few X training values:\n{np.array(training.json()['x_train'])[:5]} and here are some of the y values they were trained against:\n{np.array(training.json()['y_train'])[:5]}."
     )
-
